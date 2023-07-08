6fcff92c56b441d0768041f9d845beaec810342b
igboaventura <132757417+igboaventura@users.noreply.github.com>
2023-07-08 14:32:45 +0000

    Aula4

---
+++ turma-A/aula-0/aula4.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class aula4 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+    //    System.out.println("digite 10 valores desejados:");
+    //   //  int i = entrada.nextInt();
+    //   int maior = 0;
+
+    //     int n[]= new int [10];
+
+    //         for(int j = 0; j < 10 ; j++){
+
+    //               n[j]=  entrada.nextInt();
+                 
+    //         }
+    //         int menor = n[10-1];
+
+    //         for(int j = 0; j < 10 ; j++){
+    //             if(n[j] > maior){
+    //                 maior = n[j];
+    //             }
+    //             if(n[j] < menor){
+    //                 menor = n[j];
+    //             }
+                
+                 
+    //         }
+            
+    //         System.out.println("O maior valor é: "+ maior);
+        
+    //         System.out.println("O menor valor é: "+ menor);
+    
+
+
+        // int [][] tab={{4,6},
+        //               {1,7}};
+
+        // for(int l=0;l<2;l++){
+        //     for(int j=0;j<2;j++){
+
+        //         System.out.print(tab[l][j] + " ");
+
+        //     }
+        // }              
+
+        
+            int [][] tab  = {{3,3,3}
+                           {12,23,45}
+                           {19,23,43}};
+
+        int [][] tabt  = {{7,5,3}
+                           {12,4,49}
+                           {17,24,42}};
+   
+
+        for(int i=0;i<2;i++){
+            for(int p=0;p<2;p++){
+int soma= tab[i][j]+tabt[i][j] 
+                System.out.print(soma);
+            
+            }
+        }                
+
+    }
+}
